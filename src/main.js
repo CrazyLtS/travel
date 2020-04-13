d50b2399d0dfa46eac7b62fac6a4d0a3349c165b
CrazyLtS <59214032+CrazyLtS@users.noreply.github.com>
2020-04-13 20:53:25 +0800

    add a loop

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import router from './router'
 import fastClick from 'fastclick'
 import 'styles/reset.css'
 import 'styles/iconfont.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/component/Swiper.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="wrapper">
+    <swiper ref="mySwiper" :options="swiperOptions">
+        <swiper-slide v-for="item of swiperList" :key="item.id">
+            <img class="swiper-img" :src="item.url">
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOptions: {
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          url: 'http://img1.qunarzz.com/sight/p0/1606/68/68d9a1387d0fb3e090.water.jpg_375x170_c1fcecf1.jpg'
+        }, {
+          id: '0002',
+          url: 'http://img1.qunarzz.com/sight/p0/1606/68/68d9a1387d0fb3e090.water.jpg_375x170_c1fcecf1.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 45.3%
+    background-color: #ccc
+    .swiper-img
+      width: 100%
+</style>
