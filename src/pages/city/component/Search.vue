1c86485a2e28b0ac353b33e79d25fd78e9b35e53
CrazyLtS <59214032+CrazyLtS@users.noreply.github.com>
2020-04-16 17:39:11 +0800

    serach finish

--- src/pages/city/component/Search.vue
+++ src/pages/city/component/Search.vue
@@ -1,17 +1,77 @@
 <template>
+  <div>
     <div class="search">
-        <input class="search-input" type="text" placeholder="输入城市名或拼音">
+      <input
+       class="search-input"
+       type="text"
+       placeholder="输入城市名或拼音"
+       v-model="keyWord"
+         >
     </div>
+    <div class="search-content" ref="search" v-show="keyWord">
+      <ul>
+        <li class="search-item border-one" v-for="item of list" :key="item.id">{{item.name}}
+        </li>
+        <li class="search-item border-one" v-show="hasNoData">
+          没有找到匹配数据
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyWord: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyWord () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyWord) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyWord) > -1 || value.name.indexOf(this.keyWord) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
   @import '~styles/variables.styl'
+  .border-one
+    &::after
+      background-color: #ccc
   .search
     height: .72rem
     padding: 0 .1rem
@@ -26,4 +86,19 @@ export default {
         border: none
         border-radius: .1rem
         color: #666
+  .search-content
+    z-index: 1
+    overflow: hidden
+    position: absolute
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
+    background: #eee
+    .search-item
+      position: relative
+      line-height: .62rem
+      padding-left: .2rem
+      background-color: #fff
+      color: #666
 </style>
